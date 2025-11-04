b6338bb3b0666a8d694cb930ea8f3df27438983a
IanM <ian@morland.me>
2025-11-04 22:53:27 +0000

    fix: email notifications

--- views/email/html/status_changed.blade.php
+++ views/email/html/status_changed.blade.php
@@ -1,11 +1,7 @@
 <x-mail::html.notification>
-    <x-slot:body>
-        {!! $formatter->convert($translator->trans('ianm-twofactor.email.body.status_changed', [
+{!! $formatter->convert($translator->trans('ianm-twofactor.email.body.status_changed', [
     '{recipient_display_name}' => $user->display_name,
     '{forum_url}' => $url->to('forum')->base(),
     '{type}' => $translator->trans('ianm-twofactor.email.status_type.'.$blueprint->type())
 ])) !!}
-    </x-slot:body>
-
-    <x-slot:preview><!-- Optional content --></x-slot:preview>
 </x-mail::html.notification>
--- views/email/plain/status_changed.blade.php
+++ views/email/plain/status_changed.blade.php
@@ -1,9 +1,7 @@
 <x-mail::plain.notification>
-<x-slot:body>
 {!! $translator->trans('ianm-twofactor.email.body.status_changed', [
     '{recipient_display_name}' => $user->display_name,
     '{forum_url}' => $url->to('forum')->base(),
     '{type}' => $translator->trans('ianm-twofactor.email.status_type.'.$blueprint->type())
 ]) !!}
-</x-slot:body>
 </x-mail::plain.notification>
